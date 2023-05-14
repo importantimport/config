3333656cb5bbe4628694f6a7695741f323286c59
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-14 19:27:27 +0800

    chore(workspace): ➕ add extra config
    ➕ add @importantimport/eslint-config-extra

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import config from '@importantimport/eslint-config'
+import extra from '@importantimport/eslint-config-extra'
 import { defineFlatConfig } from 'eslint-define-config'
 import eslintPluginPlugin from 'eslint-plugin-eslint-plugin'
 
 export default defineFlatConfig([
   ...config,
+  ...extra,
   {
     files: ['./packages/eslint-config/**/*.js', './packages/eslint-config-*/**/*.js'],
     plugins: {
