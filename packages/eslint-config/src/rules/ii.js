b5a931675181c39a4a1730d6439c7dd77a354867
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-05-14 22:08:43 +0800

    refactor(eslint-config): ♻️ allow array reduce

--- packages/eslint-config/src/rules/ii.js
+++ packages/eslint-config/src/rules/ii.js
@@ -2,6 +2,8 @@
 export default {
   // disable top-level-function
   'antfu/top-level-function': 'off',
+  // allow array reduce
+  'unicorn/no-array-reduce': 'off',
   // disable check file names
   'unicorn/prevent-abbreviations': [
     'error',
