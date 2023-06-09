323ca4f5447477cab5ab7a5d677b6e1fd1bf58d2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-06-09 12:53:11 +0800

    refactor(eslint-config): ♻️ update ignores

--- packages/eslint-config/src/configs/ignores.js
+++ packages/eslint-config/src/configs/ignores.js
@@ -1,8 +1,9 @@
 /** @type {import('eslint').Linter.FlatConfig} */
 export const ignores = {
   ignores: [
-    '**/dist/*',
-    '**/node_modules/*',
+    '**/.git/**',
+    '**/dist/**',
+    '**/node_modules/**',
     '**/package-lock.json',
     '**/pnpm-lock.yaml',
     '**/yarn.lock',
