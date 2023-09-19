dc1b3bab31356c280bab52b564ec4e3fcf554e10
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-19 14:59:13 +0800

    fix(eslint-config-vanilla-extract): 🐛 import plugin

--- packages/eslint-config-vanilla-extract/src/configs/vanilla-extract.js
+++ packages/eslint-config-vanilla-extract/src/configs/vanilla-extract.js
@@ -1,12 +1,18 @@
+// plugins
+import { js } from '@importantimport/eslint-config/src/configs/js.js'
+
 // rules
 import rules from '../rules/recess-order.js'
 
 // utils
 import { GLOB_CSS_JS, GLOB_CSS_TS } from '../utils/glob.js'
 
+const { perfectionist } = /** @type {Object.<string, import('eslint').ESLint.Plugin>} */ (js.plugins)
+
 /** @type {import('eslint').Linter.FlatConfig} */
 export const vanillaExtract = {
   files: [GLOB_CSS_JS, GLOB_CSS_TS],
   ignores: ['coverage/*', 'dist/*', 'node_modules/*'],
+  plugins: { perfectionist },
   rules,
 }
