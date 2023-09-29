06a91944fd5d7e352954288bf9fca6e7d9eac152
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-29 18:11:40 +0800

    refactor(eslint-config-react): ♻️ update rules

--- packages/eslint-config-react/src/configs/react.js
+++ packages/eslint-config-react/src/configs/react.js
@@ -1,4 +1,5 @@
-import { configs as iiConfigs } from '@importantimport/eslint-config'
+import { js } from '@importantimport/eslint-config/src/configs/js.js'
+import { ts } from '@importantimport/eslint-config/src/configs/js.js'
 import { GLOB_JSX, GLOB_TSX } from '@importantimport/eslint-config/src/utils/glob.js'
 // eslint-plugin-jsx-a11y
 import jsxA11yPlugin from 'eslint-plugin-jsx-a11y'
@@ -14,47 +15,67 @@ import reactRefreshPlugin from 'eslint-plugin-react-refresh'
 // rules
 import reactRefreshRules from '../rules/react-refresh.js'
 
+/** @type {import('eslint').Linter.FlatConfig['plugins']} */
+const reactPlugins = {
+  // @ts-expect-error
+  'jsx-a11y': jsxA11yPlugin,
+  // @ts-expect-error
+  'react': reactPlugin,
+  // @ts-expect-error
+  'react-hooks': reactHooksPlugin,
+  'react-refresh': reactRefreshPlugin,
+}
+
+/** @type {import('eslint').Linter.FlatConfig['rules']} */
+// @ts-expect-error
+const reactRules = {
+  ...jsxA11yPlugin.configs.recommended.rules,
+  ...reactRecommended.rules,
+  ...reactRuntime.rules,
+  ...reactHooksPlugin.configs.recommended.rules,
+  ...reactRefreshRules,
+}
+
 /** @type {import('eslint').Linter.FlatConfig} */
 export const jsx = {
+  ...js,
   files: [GLOB_JSX],
   languageOptions: {
-    ...iiConfigs.js.languageOptions,
+    ...js.languageOptions,
     parserOptions: {
-      ...iiConfigs.js.languageOptions?.parserOptions,
+      ...js.languageOptions?.parserOptions,
       ecmaFeatures: { jsx: true },
     },
   },
   plugins: {
-    // @ts-expect-error
-    'jsx-a11y': jsxA11yPlugin,
-    // @ts-expect-error
-    'react': reactPlugin,
-    // @ts-expect-error
-    'react-hooks': reactHooksPlugin,
-    'react-refresh': reactRefreshPlugin,
+    ...js.plugins,
+    ...reactPlugins,
   },
-  // @ts-expect-error
   rules: {
-    ...jsxA11yPlugin.configs.recommended.rules,
-    ...reactRecommended.rules,
-    ...reactRuntime.rules,
-    ...reactHooksPlugin.configs.recommended.rules,
-    ...reactRefreshRules,
+    ...js.rules,
+    ...reactRules,
   },
   settings: { react: { version: '18.3' } },
 }
 
 /** @type {import('eslint').Linter.FlatConfig} */
 export const tsx = {
+  ...ts,
   files: [GLOB_TSX],
   languageOptions: {
-    ...iiConfigs.ts.languageOptions,
+    ...ts.languageOptions,
     parserOptions: {
-      ...iiConfigs.ts.languageOptions?.parserOptions,
+      ...ts.languageOptions?.parserOptions,
       ecmaFeatures: { jsx: true },
     }
   },
-  plugins: jsx.plugins,
-  rules: jsx.rules,
+  plugins: {
+    ...ts.plugins,
+    ...reactPlugins,
+  },
+  rules: {
+    ...ts.rules,
+    ...reactRules,
+  },
   settings: jsx.settings,
 }
