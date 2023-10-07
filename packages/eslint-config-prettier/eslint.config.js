b583b5b59384735915917695dcf15a11b696a133
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-10-07 20:35:16 +0800

    refactor!: 🔥 remove old packages

--- packages/eslint-config-prettier/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import { prettier } from './src/configs/prettier'
-
-/** @type {Record<string, import('eslint').Linter.FlatConfig>} */
-export const configs = {
-  prettier,
-}
-
-/** @type {import('eslint').Linter.FlatConfig[]} */
-export default Object.values(configs)
