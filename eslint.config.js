752ffc4935354e6bcd7665954dbddf9a137a0a66
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-09 22:39:37 +0800

    fix: workspace eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,17 @@
 import { createConfig } from '@importantimport/eslint-config'
 import eslintPluginPlugin from 'eslint-plugin-eslint-plugin'
 
-export default createConfig(
-  {},
-  {
-    files: ['./packages/eslint-config/**/*.js', './packages/eslint-config-*/**/*.js'],
+export default createConfig()
+  .append({
+    name: 'workspace/eslint-plugin/setup',
     plugins: {
       'eslint-plugin': eslintPluginPlugin,
     },
+  }, {
+    name: 'workspace/eslint-plugin/rules',
+    files: ['./packages/eslint-config/**/*.ts'],
     rules: {
       // @ts-ignore jesus christ
       ...eslintPluginPlugin.configs.recommended.rules,
     },
-  },
-)
+  })
