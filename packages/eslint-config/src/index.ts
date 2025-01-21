b19ede33667dd7bd9b7c870c3d3d4529484ec2f5
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-01-21 17:23:23 +0800

    refactor(eslint-config): clean

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -1,3 +1,33 @@
-export { defineConfig } from './define-config'
-export { ii, ii as importantimport, ii as default } from './factory'
-export type { Options } from './options'
+import type { Linter } from 'eslint'
+import type { ResolvableFlatConfig } from 'eslint-flat-config-utils'
+
+import antfu, { type OptionsConfig as AntfuConfig } from '@antfu/eslint-config'
+import defu from 'defu'
+
+import { disableAntfuTopLevelFunction, perfectionist, sortPackageJsonScripts } from './configs'
+
+export interface IIConfig extends AntfuConfig {
+  perfectionist: 'alphabetical' | 'line-length' | 'natural' | false
+  vanillaExtract: boolean
+}
+
+export const defaults: IIConfig = {
+  perfectionist: 'natural',
+  typescript: { tsconfigPath: './tsconfig.json' },
+  vanillaExtract: false,
+}
+
+export const ii = (userOptions: Partial<IIConfig> = {}): ResolvableFlatConfig<Linter.Config> => {
+  const options = defu(userOptions, defaults)
+
+  return [
+    disableAntfuTopLevelFunction,
+    sortPackageJsonScripts,
+    ...(options.perfectionist
+      ? [perfectionist(options.perfectionist)]
+      : []
+    ),
+  ]
+}
+
+export const defineConfig = (options: IIConfig) => antfu(options).append(ii(options))
