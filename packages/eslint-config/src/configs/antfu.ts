ce943bf4bada1ed4ab28e5470dd743c1dab3aecc
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-24 17:28:28 +0800

    refactor(eslint-config): set func-style

--- packages/eslint-config/src/configs/antfu.ts
+++ packages/eslint-config/src/configs/antfu.ts
@@ -4,5 +4,6 @@ export const disableAntfuTopLevelFunction: Linter.Config = {
   name: 'importantimport/antfu/rules',
   rules: {
     'antfu/top-level-function': 'off',
+    'func-style': ['error', 'expression'],
   },
 } as const
